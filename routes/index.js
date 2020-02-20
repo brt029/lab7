76fe802a7dfd61f13523e8b8142b09c56c8a74a8
Bradley Tran <brt029@ucsd.edu>
2020-02-20 11:50:05 -0800

    Made two separate pages view & viewAlt

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,21 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(request, response){
+	projects['viewAlt'] = false;
   	response.render('index', projects);
 };
+
+exports.viewAlt = function(request, response){
+	projects['viewAlt'] = true;
+	response.render('index', projects);
+};
+
+function initializePage() {
+	$(".likeBtn").click(clickLikeBtn);
+}
+
+function clickLikeBtn() {
+	e.preventDefault();
+	ga('create', 'UA-158839153-1', 'auto');
+	ga("send", "event", 'like', 'click');
+}
